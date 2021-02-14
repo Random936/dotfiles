9637623ef4c40d12eec38c940a1c5acbdb79921e
Random936 <randomdude936@gmail.com>
2021-02-13 17:32:03 -0800

    Added basic power10k configuration

---
+++ screenlayout/launch.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output HDMI-1 --mode 2560x1080 --pos 0x647 --rotate normal --output DP-3 --off
