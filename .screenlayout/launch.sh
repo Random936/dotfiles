1e08189024fd400d718f95208435a34d713b7432
Random936 <randomdude936@gmail.com>
2023-09-15 19:16:41 -0700

    Updating config based on linux laptop

--- .screenlayout/launch.sh
+++ .screenlayout/launch.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-xrandr --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output HDMI-1 --mode 2560x1080 --pos 0x647 --rotate normal --output DP-3 --off
+#!/bin/bash
+
+script="$HOME/.screenlayout/layout.sh"
+
+if [ -f "$script" ]; then
+   $script
+fi
+
+nitrogen --restore
