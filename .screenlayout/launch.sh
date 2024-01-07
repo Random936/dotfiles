2470afaefca01ece0e7cd2f3960eab4b592ac082
Jaden Provost Maxwell-Comfort <randomdude936@gmail.com>
2024-01-06 16:09:20 -0800

    Updated the screen layout/wallpaper config

--- .screenlayout/launch.sh
+++ .screenlayout/launch.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Load screen layout script if it exists
 script="$HOME/.screenlayout/layout.sh"
-
 if [ -f "$script" ]; then
    $script
 fi
 
-feh --bg-scale $HOME/Pictures/wallpaper.jpg
+# Dynamically load background with feh
+feh_command="feh --bg-fill"
+while read resolution; do
+    feh_command="$feh_command ~/Pictures/wallpaper-$resolution.jpg"
+done < <(xrandr | grep ' connected' | grep -oP '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' | cut -d '+' -f 1)
+
+echo "$feh_command"
+eval "$feh_command"
