5974ae879e010e81ff7cd9b1d4425e632bd79b40
Random936 <randomdude936@gmail.com>
2024-02-14 21:38:42 -0800

    Created dedicated wallapers.sh script and added betterlockscreen to i3

--- .screenlayout/launch.sh
+++ .screenlayout/launch.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Load screen layout script if it exists
-script="$HOME/.screenlayout/layout.sh"
-if [ -f "$script" ]; then
-   $script
-fi
+function load_script() {
+    if [ -f "$1" ]; then
+        $1
+    fi
+}
 
-# Dynamically load background with feh
-feh_command="feh --bg-fill"
-while read resolution; do
-    feh_command="$feh_command ~/Pictures/wallpaper-$resolution.jpg"
-done < <(xrandr | grep ' connected' | grep -oP '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' | cut -d '+' -f 1)
+load_script "$HOME/.screenlayout/layout.sh" # screen layout script
+load_script "$HOME/.screenlayout/wallpapers.sh" # wallpapers script
 
-echo "$feh_command"
-eval "$feh_command"
---
+++ .screenlayout/wallpapers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Dynamically load background with feh
+feh_command="feh --bg-fill"
+while read resolution; do
+    feh_command="$feh_command ~/Pictures/wallpaper-$resolution.jpg"
+done < <(xrandr | grep ' connected' | grep -oP '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' | cut -d '+' -f 1)
+
+echo "$feh_command"
+eval "$feh_command"
