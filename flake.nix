ac9bc5772e3a7ad9205f63decb28ff1218d29593
Random936 <randomdude936@gmail.com>
2024-05-17 15:22:37 -0700

    Moved dotfiles to new location for switch to nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Configuration flake for RandomCTF";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations.randomctf = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [ ./nixos/configuration.nix ];
+      };
+
+      homeConfigurations.random = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [ ./home-manager/home.nix ];
+      };
+    };
+}
