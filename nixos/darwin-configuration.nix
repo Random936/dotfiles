b77f0d436c66ff6ab3a5fb586ac9346e188cb175
Random936 <randomdude936@gmail.com>
2024-06-04 12:58:12 -0700

    Added mouse and keyboard configuration to nix-darwin config

--- nixos/darwin-configuration.nix
+++ nixos/darwin-configuration.nix
@@ -32,6 +32,14 @@
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
+  # Keyboard and Mouse
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+  system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = true;
+
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
 
