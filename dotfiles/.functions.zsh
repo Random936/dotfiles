6a3503794ce3be7427ac2aac7caf53dc42805851
Random936 <randomdude936@gmail.com>
2024-06-14 11:22:49 -0700

    Updated bitwarden-cli integration to mbsync

--- dotfiles/.functions.zsh
+++ dotfiles/.functions.zsh
@@ -1,10 +1,19 @@
-bw-init() {
-    bw login --check &>/dev/null || return 0
-    bw sync &>/dev/null || return 0
-
-    bw unlock --raw > $HOME/.bw_session
-}
-
 bw-load() {
     export BW_SESSION=$(cat $HOME/.bw_session)
+}
+
+bw-init() {
+    if [ ! bw login --check &>/dev/null ]; then
+        bw login
+    fi
+
+    bw sync &>/dev/null || return 1
+    bw-load
+
+    if [[ "$(bw status | jq -rc .status)" == "unlocked" ]]; then
+        return 0
+    fi
+
+    bw unlock --raw > $HOME/.bw_session
+    bw-load
 }
