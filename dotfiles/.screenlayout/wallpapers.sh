0814b01de8d93ba5ea75ae672ee545cba86960e8
Random936 <randomdude936@gmail.com>
2024-07-12 22:05:31 -0700

    Fixed bug in wallpaper.sh screenlayout script

--- dotfiles/.screenlayout/wallpapers.sh
+++ dotfiles/.screenlayout/wallpapers.sh
@@ -4,7 +4,7 @@
 feh_command="feh --bg-fill"
 while read resolution; do
     feh_command="$feh_command ~/Pictures/wallpaper-$resolution.jpg"
-done < <(xrandr | grep ' connected' | grep -oP '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' | cut -d '+' -f 1)
+done < <(xrandr | grep ' connected' | grep -oP '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' | sort -t+ -nk3 | cut -d '+' -f 1)
 
 echo "$feh_command"
 eval "$feh_command"
