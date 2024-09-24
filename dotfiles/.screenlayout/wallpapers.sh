2678527726d38cb99253e1dd304f4c5cce19d6e5
Random936 <randomdude936@gmail.com>
2024-09-23 22:10:49 -0700

    Hopefully fixed wallpapers.sh script

--- dotfiles/.screenlayout/wallpapers.sh
+++ dotfiles/.screenlayout/wallpapers.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Dynamically load background with feh
-feh_command="feh --bg-fill"
+feh_command="feh"
 while read resolution; do
-    feh_command="$feh_command ~/Pictures/wallpaper-$resolution.jpg"
-done < <(xrandr | grep ' connected' | grep -oP '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' | sort -t+ -nk3 | cut -d '+' -f 1)
+    echo "Assigning wallpaper-$resolution.jpg to $(xrandr | grep ' connected' | grep $resolution | awk '{print $1}')"
+    feh_command="$feh_command --bg-fill ~/Pictures/wallpaper-$resolution.jpg"
+done < <(xrandr --listmonitors | grep -oP '[0-9]+\/[0-9]+x[0-9]+\/[0-9]+' | awk -F'[/x]' '{printf "%sx%s\n", $1, $3}')
 
 echo "$feh_command"
 eval "$feh_command"
