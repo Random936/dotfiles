ae353630903e580eb78e43c2ad1993a24ba7b983
Random936 <randomdude936@gmail.com>
2025-01-07 14:25:13 -0800

    Possible fix for networking issues on VMs

--- config/networking.nix
+++ config/networking.nix
@@ -1,21 +1,22 @@
-{ ip_address, hostname, open_ports, lib }:
-{
+{ ip_address, hostname, open_ports, lib }: let
+  gateway_ip = "192.168.100.1";
+in {
   # Configure hostname.
   networking.hostName = hostname;
 
   # Conigure a static IP address.
   networking.useDHCP = false;
-  networking.useNetworkd= true;
+  networking.useNetworkd = true;
 
   # Bug fix
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
   networking.defaultGateway = {
-      address = ip_address;
+      address = gateway_ip;
       interface = "enp6s18";
   };
 
-  networking.nameservers = [ "192.168.100.1" ];
+  networking.nameservers = [ gateway_ip ];
   networking.firewall.allowedTCPPorts = open_ports;
   networking.interfaces.enp6s18.ipv4.addresses = [
     {
